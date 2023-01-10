278261d476656c318f6798548324bad95eb5cf0d
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-10 04:40:47 +0000

    "Tail"

--- tail.js
+++ tail.js
@@ -1,6 +1,29 @@
-const assertEqual = function (tail) {
-  console.log(tail.slice(1))
-}
+const assertEqual = function(actual, expected) {
+  if(actual !== expected){
+    console.log("fail");
+  } else{
+    console.log("Pass");
+  } 
+};  
 
-assertEqual(['Hello', 'Lighthouse', 'Labs'])
-assertEqual([5, 6, 7])
+
+
+function tail(array) {
+  console.log("Array", array)
+  return array.slice(1)
+
+}
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); 
+assertEqual(words.length, 3);
+
+
+const oneElement = tail(['Hello'])
+assertEqual(oneElement.length, 0)
+const anotherElement = tail([])
+assertEqual(anotherElement.length,0)
