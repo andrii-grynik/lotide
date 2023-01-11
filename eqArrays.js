2dda856e1ecef52024aa93123c4f87e68dd89fc1
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-11 22:54:49 +0000

    "eqArrays 2.0"

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,25 @@
-const eqArrays = function(array1, array2) {
-  let areEqual = true;
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log('fail')
+  } else {
+    console.log('Pass')
+  }
+}
+
+const eqArrays = function (array1, array2) {
+  let areEqual = true
 
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i] || array1.length !== array2.length) {
-      areEqual = false;
+      areEqual = false
     }
   }
-  return areEqual;
-};
+  return areEqual
+}
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], ["1", "2", "3"]));
+
+//Edge Cases
+//console.log(eqArrays([1, 2, 3], [1, 2, 3]))
+//console.log(eqArrays([1, 2, 3], ['1', '2', '3']))
+
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => should PASS
