4e0124595611b6612584af6fbff4f03398a74ede
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-11 23:18:45 +0000

    "Middle"

--- middle.js
+++ middle.js
@@ -1,20 +1,41 @@
-const middle = function (array) {
-  const returnArray = [];
+const eqArrays = function (array1, array2) {
+  let areEqual = true
 
-  if (array.length > 2) {
-    const middle = array.length / 2 - 1;
-    if (array.length % 2 !== 0) {
-      returnArray.push((array[Math.ceil(middle)]));
-    } else {
-      returnArray.push(array[middle], array[middle + 1]);
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] || array1.length !== array2.length) {
+      areEqual = false
     }
-    return returnArray;
+  }
+  return areEqual
+}
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`${array1} === ${array2}\nThese arrays are equal :)`)
   } else {
-    return [];
+    console.log(`${array1} !== ${array2}\nThese arrays are not equal :(`)
   }
 }
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+const middle = function (array) {
+  const returnArray = []
+
+  if (array.length > 2) {
+    const middle = array.length / 2 - 1
+    if (array.length % 2 !== 0) {
+      returnArray.push((array[Math.ceil(middle)]))
+    } else {
+      returnArray.push(array[middle], array[middle + 1])
+    }
+    return returnArray
+  } else {
+    return []
+  }
+}
+
+// Edge cases
+
+// console.log(middle([1, 2, 3])) // => [2]
+// console.log(middle([1, 2, 3, 4, 5])) // => [3]
+// console.log(middle([1, 2, 3, 4])) // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
