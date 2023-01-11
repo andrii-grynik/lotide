2af8489c799007bcc61aede497f49d774b2490fe
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-10 19:36:39 -0700

    Create eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,15 @@
+const eqArrays = function(array1, array2) {
+  let areEqual = true;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] || array1.length !== array2.length) {
+      areEqual = false;
+    }
+  }
+  return areEqual;
+};
+
+module.exports = eqArrays;
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], ["1", "2", "3"]));
