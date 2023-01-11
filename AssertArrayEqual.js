15ba6aa02fa347de43aa531c053ba5d911bbcc3f
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-11 23:04:36 +0000

    "AssertArraysEqual 2.0"

--- AssertArrayEqual.js
+++ AssertArrayEqual.js
@@ -1,23 +1,33 @@
-const eqArrays = function(array1, array2) {
-  let areEqual = true;
+const assertEqual = function (actual, expected) {
+  if (actual !== expected) {
+    console.log('fail')
+  } else {
+    console.log('Pass')
+  }
+}
+
+const eqArrays = function (array1, array2) {
+  let areEqual = true
 
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      areEqual = false;
+    if (array1[i] !== array2[i] || array1.length !== array2.length) {
+      areEqual = false
     }
   }
-  return areEqual;
-};
+  return areEqual
+}
 
-const assertArraysEqual = function(array1, array2) {
+const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`${array1} === ${array2}\nThese arrays are equal :)`);
+    console.log(`${array1} === ${array2}\nThese arrays are equal :)`)
   } else {
-    console.log(`${array1} !== ${array2}\nThese arrays are not equal :(`);
+    console.log(`${array1} !== ${array2}\nThese arrays are not equal :(`)
   }
-};
+}
 
-module.exports = assertArraysEqual;
+// Edge Cases
+//console.log(eqArrays([1, 2, 3], [1, 2, 3]))
+//console.log(eqArrays([1, 2, 3], ['1', '2', '3']))
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], ["1", "2", "3"]));
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => should PASS
+//assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
