ab97f3e6be21e985d57bcfc36c2c4e79bdff868e
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-12 00:01:38 +0000

    "Letter Positions"

--- letterPositions.js
+++ letterPositions.js
@@ -33,8 +33,8 @@ const letterPositions = function (string) {
   }
   return results
 }
-
-assertArraysEqual(letterPositions('hello').h, [0])
-assertArraysEqual(letterPositions('hello').e, [1])
-assertArraysEqual(letterPositions('hello').l, [2, 3])
-assertArraysEqual(letterPositions('hello').o, [4])
+// edge cases
+//assertArraysEqual(letterPositions('hello').h, [0])
+//assertArraysEqual(letterPositions('hello').e, [1])
+//assertArraysEqual(letterPositions('hello').l, [2, 3])
+//assertArraysEqual(letterPositions('hello').o, [4])
