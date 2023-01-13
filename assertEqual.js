0df9cf50ac035af273012130f1f8a660149f9872
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-12 19:58:42 -0700

    Update assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if(actual !== expected){
-    console.log("fail")
-  } else{
-    console.log("Pass")
-  } 
-}  
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`< (o_O) (o_O) (o_O) > Assertion Passed: ${actual} === ${expected}`) 
+  } else {
+    console.log(`< (T_T) (T_T) (T_T) > Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
 
 //Edge Cases
 //assertEqual("Lighthouse Labs", "Bootcamp");
