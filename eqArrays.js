0572ed6b2501c4d5c3f8393639583c526f0d5b47
Andrii <95922075+andrii-grynik@users.noreply.github.com>
2023-01-17 03:29:21 +0000

    "reformats"

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`< (o_O) (o_O) (o_O) > Assertion Passed: ${actual} === ${expected}`)
-  } else {
-    console.log(`< (T_T) (T_T) (T_T) > Assertion Failed: ${actual} !== ${expected}`)
-  }
-}
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function (array1, array2) {
   let areEqual = true
@@ -17,9 +11,5 @@ const eqArrays = function (array1, array2) {
   return areEqual
 }
 
-/* Edge Cases
-console.log(eqArrays([1, 2, 3], [1, 2, 3]))
-console.log(eqArrays([1, 2, 3], ['1', '2', '3']))
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => should PASS
-*/
+module.exports = eqArrays;
--- tail.js
+++ tail.js
@@ -6,21 +6,3 @@ function tail (array) {
 }
 
 module.export = tail;
-
-
-/* Edge Cases
-
-const result = tail(['Hello', 'Lighthouse', 'Labs'])
-assertEqual(result.length, 2) // ensure we get back two elements
-assertEqual(result[0], 'Lighthouse') // ensure first element is "Lighthouse"
-assertEqual(result[1], 'Labs')
-
-const words = ['Yo Yo', 'Lighthouse', 'Labs']
-tail(words)
-assertEqual(words.length, 3)
-
-const oneElement = tail(['Hello'])
-assertEqual(oneElement.length, 0)
-const anotherElement = tail([])
-assertEqual(anotherElement.length, 0)
-*/
